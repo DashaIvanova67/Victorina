21dd700b03d5ea493645d025a7744eb241070c9d
an di <an@di.ru>
2022-05-19 14:36:19 +0300

    main

--- app/src/main/java/com/example/victorina/MainActivity.java
+++ app/src/main/java/com/example/victorina/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.victorina;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -17,5 +18,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void openTest(View view) {
         startActivity(new Intent(MainActivity.this, VictorinaActivity.class));
+    }
+
+    public void redact(View view) {
+        startActivity(new Intent(MainActivity.this, AdminActivity.class));
     }
 }
